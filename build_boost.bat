7c3324946756bc7a149721820ca832f63975bfc0
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2015-12-29 12:33:26 -0500

    building boost

---
+++ build_boost.bat
@@ -0,0 +1,9 @@
+cd Dependencies
+cd boost
+SET VS_UNICODE_OUTPUT=""
+SET _LIBRARIES= --with-chrono --with-date_time --with-filesystem --with-program_options --with-exception --with-math --with-regex --with-system --with-thread --with-test
+SET _PARAMS= runtime-link=static threading=multi link=static --layout-tagged
+SET _FLAGS="/wd4800 /wd4244 /wd4996 /wd4273 /wd4702 /wd4251"
+boostrap
+b2 toolset=msvc %_PARAMS% variant=debug address-model=32 %_LIBRARIES% stage cxxflags=%_FLAGS%
+b2 install
