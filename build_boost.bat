52218742d7440820cc117ca54c9bfba606cb4de3
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2016-01-01 15:56:51 +0000

    removing exception and math libs

--- build_boost.bat
+++ build_boost.bat
@@ -1,7 +1,7 @@
 cd Dependencies
 cd boost
 SET VS_UNICODE_OUTPUT=""
-SET _LIBRARIES= --with-chrono --with-date_time --with-filesystem --with-program_options --with-exception --with-math --with-regex --with-system --with-thread --with-test
+SET _LIBRARIES= --with-chrono --with-date_time --with-filesystem --with-program_options --with-regex --with-system --with-thread --with-test
 SET _PARAMS= runtime-link=static threading=multi link=static --layout-tagged
 SET _FLAGS="/wd4800 /wd4244 /wd4996 /wd4273 /wd4702 /wd4251"
 boostrap
