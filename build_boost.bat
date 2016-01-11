ec21ee8a1a1bf3152f5d606ab4b39c5687edac48
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2016-01-11 17:42:17 -0500

    fix typo on bootstrap

--- build_boost.bat
+++ build_boost.bat
@@ -4,6 +4,6 @@ SET VS_UNICODE_OUTPUT=""
 SET _LIBRARIES= --with-date_time --with-filesystem --with-iostreams --with-python --with-regex --with-system --with-test
 SET _PARAMS= runtime-link=static threading=multi link=static --layout-tagged
 SET _FLAGS="/wd4800 /wd4244 /wd4996 /wd4273 /wd4702 /wd4251"
-boostrap
+bootstrap
 b2 toolset=msvc %_PARAMS% variant=debug address-model=32 %_LIBRARIES% stage cxxflags=%_FLAGS%
 b2 install
